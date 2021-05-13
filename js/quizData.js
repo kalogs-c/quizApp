24c0930bd7d6f95e7ab99a88d9f6b9566b2a601f
Carlos Henrique Camilo <carloscamilocontato@gmail.com>
2021-05-13 14:01:29 -0300

    html css done

---
+++ js/quizData.js
@@ -0,0 +1,42 @@
+const quizData = [
+    {
+        question: 'How old is Florin?',
+        a: '10',
+        b: '15',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    }, 
+    {
+        question: 'What is the most used programming language in 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'd'
+    },
+    {
+        question: 'Who is the president of US?',
+        a: 'Florian Pop',
+        b: 'Donald Trump',
+        c: 'Kalogs',
+        d: 'Ivan Soldano',
+        correct: 'b'
+    },
+    { 
+        question: 'What does HTML stand for?',
+        a: 'Hyper text markup language',
+        b: 'Hyper turbo markup language',
+        c: 'Hoper type mini language',
+        d: 'Hyper text markup loser',
+        correct: 'a'
+    },
+    {
+        question: 'What year was JavaScript launched?',
+        a: '1996',
+        b: '1992',
+        c: '1994',
+        d: 'None of above',
+        correct: 'd'
+    }
+]
