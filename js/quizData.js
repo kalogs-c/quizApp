26bfd4038be1c01efd90625e6358bb5a554db491
Carlos Henrique Camilo <carloscamilocontato@gmail.com>
2021-05-14 15:09:32 -0300

    perguntas novas

--- js/quizData.js
+++ js/quizData.js
@@ -1,42 +1,42 @@
 const quizData = [
     {
-        question: 'How old is Florin?',
-        a: '10',
-        b: '15',
-        c: '26',
-        d: '110',
-        correct: 'c'
-    },
-    {
-        question: 'What is the most used programming language in 2019?',
-        a: 'Java',
-        b: 'C',
-        c: 'Python',
-        d: 'JavaScript',
-        correct: 'd'
-    },
-    {
-        question: 'Who is the president of US?',
-        a: 'Florian Pop',
-        b: 'Donald Trump',
-        c: 'Kalogs',
-        d: 'Ivan Soldano',
-        correct: 'b'
-    },
-    {
-        question: 'What does HTML stand for?',
-        a: 'Hyper text markup language',
-        b: 'Hyper turbo markup language',
-        c: 'Hoper type mini language',
-        d: 'Hyper text markup loser',
+        question: 'Quais o menor e o maior país do mundo?',
+        a: 'Vaticano e Rússia',
+        b: 'Nauru e China',
+        c: 'Mônaco e Canadá',
+        d: 'Malta e Estados Unidos',
         correct: 'a'
     },
     {
-        question: 'What year was JavaScript launched?',
-        a: '1996',
-        b: '1992',
-        c: '1994',
-        d: 'None of above',
+        question: 'O que a palavra legend significa em português?',
+        a: 'História',
+        b: 'Conto',
+        c: 'Legenda',
+        d: 'Lenda',
         correct: 'd'
+    },
+    {
+        question: 'Quanto tempo a luz do Sol demora para chegar à Terra?',
+        a: '1 hora',
+        b: '8 minutos',
+        c: '0.254 segundos',
+        d: '1 milisegundo',
+        correct: 'b'
+    },
+    {
+        question: 'Qual o jogo mais vendido do mundo?',
+        a: 'GTA San Andreas',
+        b: 'Super Mario World',
+        c: 'Minecraft',
+        d: 'Tetris',
+        correct: 'c'
+    },
+    {
+        question: 'Qual o filme com a maior bilheteria da história?',
+        a: 'Star Wars: O despertar da força',
+        b: 'Titanic',
+        c: 'Avatar',
+        d: 'Vingadores: Ultimato',
+        correct: 'c'
     }
 ]
